a2fee52e6ad6ed537e689412ca498092ceaf7ff9
ab13abhi <ashu.world123@gmail.com>
2023-03-10 16:00:16 +0530

    .

---
+++ WEEK 5 CHAR ARRAY & STRINGS/hw_index_of_first_occurance.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int strStr(string haystack, string needle)
+{
+    int n = haystack.size();
+    int m = needle.size();
+
+    for (int i = 0; i <= n - m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (needle[j] != haystack[i + j])
+            {
+                break;
+            }
+            if (j == m - 1)
+            {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+int main()
+{
+
+    // string haystack = "sadbutsad", needle = "sad";
+    string haystack = "leetcode", needle = "leeto";
+    cout << strStr(haystack, needle);
+    return 0;
+}
